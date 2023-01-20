37f361220a7d29d64c0709cd2595286578b1588a
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-01-20 16:52:05 -0600

    Added variables in JS according to https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+$(function() {
+    const APIKey = "f3590b94654981da9b8d1099d19b0979";
+    let city;
+    let state;
+    let country;
+    const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+
+
+
+
+
+
+
+})
