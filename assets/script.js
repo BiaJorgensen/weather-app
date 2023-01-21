c5869c7dc97572ac6774237d5f0a76f75a90d6b7
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-01-21 15:58:32 -0600

    Added variable to house forecast URL

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,8 @@
 $(function() {
     const APIKey = "f3590b94654981da9b8d1099d19b0979";
     let city;
-    let state;
-    let country;
-    const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    const queryURLcurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    const queryURLforecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5" + "&appid=" + APIKey
 
 
 
