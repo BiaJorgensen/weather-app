6716d81e9a0c10fed7febdccd8839bf6e2f66bdb
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-01-21 18:44:44 -0600

    Created function to get current weather info - only showing in console for now

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,20 @@
+
+const APIKey = "f3590b94654981da9b8d1099d19b0979";
+
 $(function() {
-    const APIKey = "f3590b94654981da9b8d1099d19b0979";
     
-    // const queryURLcurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    
+    
     // const queryURLforecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey
     
     $('#search').click(function (e) { 
         e.preventDefault();
-        const city = $('#citySearch').val()
+        const city = $('#citySearch').val();
         localStorage.setItem('city', JSON.stringify(city));
         const testCity = JSON.parse(localStorage.getItem('city'))
         if (testCity) {
             console.log(testCity);
+            getCurrentWeather()
         }
         else {
             console.log('false');
@@ -18,8 +22,19 @@ $(function() {
        
     
     });
+    
 })
 
-
+function getCurrentWeather() {
+    const city = JSON.parse(localStorage.getItem('city'))
+    const queryURLcurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + APIKey;
+    fetch(queryURLcurrent)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+          });
+}
 
 
