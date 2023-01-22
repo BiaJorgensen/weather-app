d56840a1c97ea01eced530020f6631f4e56c8f41
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-01-21 18:14:28 -0600

    Created test function to save city in local storage when button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,25 @@
 $(function() {
     const APIKey = "f3590b94654981da9b8d1099d19b0979";
-    let city;
-    const queryURLcurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-    const queryURLforecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5" + "&appid=" + APIKey
-
-
-
-
-
-
-
-
-
+    
+    // const queryURLcurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    // const queryURLforecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey
+    
+    $('#search').click(function (e) { 
+        e.preventDefault();
+        const city = $('#citySearch').val()
+        localStorage.setItem('city', JSON.stringify(city));
+        const testCity = JSON.parse(localStorage.getItem('city'))
+        if (testCity) {
+            console.log(testCity);
+        }
+        else {
+            console.log('false');
+        }
+       
+    
+    });
 })
+
+
+
+
